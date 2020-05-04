a3a141c37cc301e4e9f1edfea55eb4c414a3f5a1
Patrick Flor <git.push.origin@gmail.com>
2020-05-04 02:03:10 -0500

    ⌨️ Add typed header to substantiate "anything"

--- components/web/src/Gyro.js
+++ components/web/src/Gyro.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
-
+import styled from 'styled-components'
+import Typist from 'react-typist'
+import 'react-typist/dist/Typist.css'
 import Mouse from './Mouse'
 import Rocket from './Rocket'
 
+const Typed = styled.span`
+  font-family: Inconsolata, monospace;
+  font-weight: 900;
+`
+
+const TypedNoteWrapper = styled.div`
+  margin: 0;
+  text-align: center;
+`
+
+const SmallNote = styled.span`
+  font-family: Inconsolata, monospace;
+  font-weight: 900;
+  font-size: 1.0rem;
+`
 
 class Gyro extends Component {
+
+  state = {
+    readyForNote: false
+  }
 
   // TODO: Remove knowledge of #main-header from this component
   handleClickToScroll = () => {
@@ -18,16 +39,46 @@ class Gyro extends Component {
     })
   }
 
+  handleFinishType = () => {
+    this.setState({
+      readyForNote: true
+    })
+  }
+
   render() {
     return <div id='gyro'>
       <div id='gyro-text'>
         <div>
-          <h2>Click once,<br />build anything</h2>
+          <h2>The fastest way to</h2>
+          <h2><Typist cursor={{
+            hideWhenDone: true,
+            hideWhenDoneDelay: 0,
+          }} onTypingDone={this.handleFinishType}>
+            <Typed>create a website</Typed>
+            <Typist.Backspace count={16} delay={900} />
+            <Typist.Delay ms={500} />
+            <Typed>build a full-stack app</Typed>
+            <Typist.Backspace count={22} delay={900} />
+            <Typist.Delay ms={500} />
+            <Typed>share a slide deck</Typed>
+            <Typist.Backspace count={18} delay={900} />
+            <Typist.Delay ms={500} />
+            ship anything
+          </Typist></h2>
+          {this.state.readyForNote ? (
+            <TypedNoteWrapper><Typist cursor={{
+              hideWhenDone: true,
+              hideWhenDoneDelay: 0,
+            }} >
+              <SmallNote>(custom templates coming soon!)</SmallNote>
+            </Typist></TypedNoteWrapper>
+          ) : null
+          }
           <div id='gyro-emoji'>
             <Mouse />
             <Rocket />
           </div>
-          <p>Platter helps developers and businesses quickly <span className='keyword'>ship</span>, <span className='keyword'>operate</span>, and <span className='keyword'>scale</span> products across clouds and user platforms.</p>
+          <p>Platter gives you one-click templates to <span className='keyword'>create</span>, <span className='keyword'>deploy</span>, and <span class='keyword'>scale</span> new projects and products, now.</p>
         </div>
       </div>
       <div id='gyro-down'><button onClick={this.handleClickToScroll }>▼</button></div>
