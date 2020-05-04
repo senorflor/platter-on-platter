031caeabddec55bffff8f9bcad1dff2a8a8e6030
Patrick Flor <git.push.origin@gmail.com>
2020-05-03 21:53:28 -0500

    ©️ Add current year to copyright notice

--- components/web/src/Footer.js
+++ components/web/src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => <footer>
   <div>
     <p>Platter is made with <span role="img" aria-label="heart">❤️</span> in Remote, USA.</p>
-    <p><small>© 2019 Boilerplatter, Inc. All rights reserved.</small></p>
+    <p><small>© 2019-2020 Boilerplatter, Inc. All rights reserved.</small></p>
   </div>
 </footer>
 
