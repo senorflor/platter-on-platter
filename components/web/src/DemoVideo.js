48f2a978767f294de35d1b2b7b6b19c663b441d1
Patrick Flor <git.push.origin@gmail.com>
2020-05-05 03:26:29 -0500

    🎥 Add new demo video

--- components/web/src/DemoVideo.js
+++ components/web/src/DemoVideo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const DemoVideo = () => <div id="demoVideo">
   <iframe
     title="Platter demo"
-    src="https://www.loom.com/embed/c073ec4e6b2146a29658f4c611835b18"
+    src="https://www.loom.com/embed/7481ac0931ba481ebc40bfd9d10694e0"
     frameBorder="0"
     allowFullScreen={true}>
   </iframe>
