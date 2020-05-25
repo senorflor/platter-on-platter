c9db70069f837f744cc66b7e6050ee35ace35848
Patrick Flor <git.push.origin@gmail.com>
2020-05-25 01:39:02 -0500

    ♻️ Hooks-ify App.js

--- components/web/src/App.js
+++ components/web/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import throttle from 'lodash.throttle'
 
 import Cta from './Cta'
@@ -9,53 +9,47 @@ import Header from './Header'
 import Instructions from './Instructions'
 import './App.css'
 
+// Pixels before we make header transition to opaque
+const scrollThreshold = 6
 
-class App extends Component {
+const App = () => {
 
-  scrollThreshold = 6 // Pixels before we make header transition to opaque
+  const [ scrolled, updateScrolled ] = useState(window.pageYOffset > scrollThreshold)
 
-  state = {
-    scrolled: window.pageYOffset > 5,
-  }
-
-  handleScroll = throttle(
-    (e) => {
-      this.setState({
-        scrolled: window.pageYOffset > this.scrollThreshold
-      })
-    },
-    100,
-    {
-      leading: true,
-      trailing: true,
+  useEffect(() => {
+    const handleScroll = throttle(
+      (e) => {
+        updateScrolled(
+          window.pageYOffset > scrollThreshold
+        )
+      },
+      100,
+      {
+        leading: true,
+        trailing: true,
+      }
+    )
+    window.addEventListener('scroll', handleScroll, true)
+    return function cleanupScrollListener() {
+      window.removeEventListener('scroll', handleScroll)
     }
-  )
+  }) 
 
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll, true)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll)
-  }
-
-  render() {
-    return (
-      <>
-        <Header scrolled={this.state.scrolled} />
-        <div id='main'>
-          <Gyro/>
-          <div id='how-it-works'>
-            <h2>how it works</h2>
-            <Instructions/>
-            <DemoVideo/>
-            <Cta />
-          </div>
+  return (
+    <>
+      <Header scrolled={scrolled} />
+      <div id='main'>
+        <Gyro/>
+        <div id='how-it-works'>
+          <h2>how it works</h2>
+          <Instructions/>
+          <DemoVideo/>
+          <Cta />
         </div>
-        <Footer/>
-      </>
-    );
-  }
+      </div>
+      <Footer/>
+    </>
+  )
 }
 
 export default App
